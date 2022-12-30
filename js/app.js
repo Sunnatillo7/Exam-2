80b06d8c7ba60611a99bfd90fe394b23a99c86a1
Your Name <you@example.com>
2022-12-30 17:15:54 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,68 @@
+var elDropTop = document.querySelector(".drops__top");
+var elTextWrapper = document.querySelector(".drops__text");
+var elDropIcon = document.querySelector(".drops__img");
+elDropTop.addEventListener("click", function () {
+    elTextWrapper.classList.toggle("show-text");
+    elDropIcon.classList.toggle("turn");
+
+});
+
+
+
+
+var elDropTop2 = document.querySelector(".drops__top2");
+var elTextWrapper2 = document.querySelector(".drops__text2");
+var elDropIcon2 = document.querySelector(".drops__img2");
+elDropTop2.addEventListener("click", function () {
+    elTextWrapper2.classList.toggle("show-text2");
+    elDropIcon2.classList.toggle("turn2");
+
+});
+
+
+
+
+var elDropTop3 = document.querySelector(".drops__top3");
+var elTextWrapper3 = document.querySelector(".drops__text3");
+var elDropIcon3 = document.querySelector(".drops__img3");
+elDropTop3.addEventListener("click", function () {
+    elTextWrapper3.classList.toggle("show-text3");
+    elDropIcon3.classList.toggle("turn3");
+
+});
+
+
+
+
+var elDropTop4 = document.querySelector(".drops__top4");
+var elTextWrapper4 = document.querySelector(".drops__text4");
+var elDropIcon4 = document.querySelector(".drops__img4");
+elDropTop4.addEventListener("click", function () {
+    elTextWrapper4.classList.toggle("show-text4");
+    elDropIcon4.classList.toggle("turn4");
+
+});
+
+
+
+
+var elDropTop5 = document.querySelector(".drops__top5");
+var elTextWrapper5 = document.querySelector(".drops__text5");
+var elDropIcon5 = document.querySelector(".drops__img5");
+elDropTop5.addEventListener("click", function () {
+    elTextWrapper5.classList.toggle("show-text5");
+    elDropIcon5.classList.toggle("turn5");
+
+});
+
+
+
+
+var elDropTop6 = document.querySelector(".drops__top6");
+var elTextWrapper6 = document.querySelector(".drops__text6");
+var elDropIcon6 = document.querySelector(".drops__img6");
+elDropTop6.addEventListener("click", function () {
+    elTextWrapper6.classList.toggle("show-text6");
+    elDropIcon6.classList.toggle("turn6");
+
+});
